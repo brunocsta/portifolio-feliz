039552bb0d594840dfb3aeb83d37e373ee0a9aaf
Bruno Costa <b.costamartins@gmail.com>
2024-06-20 13:13:00 -0300

    Fix the nav bug

--- js/components/main-nav.js
+++ js/components/main-nav.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector('header.main-header .navigation');
 const iconClose = document.querySelector('.icon-close');
+const carousel = document.querySelector('.main-carousel');
 const iconMenu = document.querySelector('.icon-menu');
 const mediaQuery = window.matchMedia("(max-width: 980px)");
 
@@ -7,12 +8,18 @@ mediaQuery.addEventListener('change', (Event) => {
     if (Event.matches){
         iconMenu.addEventListener('click', function() {
             nav.style = 'display: flex';
-        })
+            carousel.style = 'display: none';
+        });
 
         iconClose.addEventListener('click' ,function() {
             nav.style = 'display: none';
+            carousel.style = 'display: flex';
+
         })
         
+    }
+    else { 
+        nav.style = 'display: flex';
     }
 })
 
