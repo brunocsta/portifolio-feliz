f89ff91885a564ad7c2f0df80374d408bd38ab77
Bruno Costa <b.costamartins@gmail.com>
2024-06-20 13:02:33 -0300

    Add a matchMedia()

--- js/components/main-nav.js
+++ js/components/main-nav.js
@@ -1,6 +1,20 @@
 const nav = document.querySelector('header.main-header .navigation');
 const iconClose = document.querySelector('.icon-close');
+const iconMenu = document.querySelector('.icon-menu');
+const mediaQuery = window.matchMedia("(max-width: 980px)");
 
-iconClose.addEventListener('click' ,function() {
-    nav.style = 'display: none';
+mediaQuery.addEventListener('change', (Event) => {
+    if (Event.matches){
+        iconMenu.addEventListener('click', function() {
+            nav.style = 'display: flex';
+        })
+
+        iconClose.addEventListener('click' ,function() {
+            nav.style = 'display: none';
+        })
+        
+    }
 })
+
+
+
